939978785f6c12176e278284607df6353c9e2962
samuelsbrn <142556206+samuelsbrn@users.noreply.github.com>
2026-03-10 08:48:58 +0700

    user 2

--- src/components/PublicLayout.jsx
+++ src/components/PublicLayout.jsx
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 
 export default function PublicLayout() {
+  const location = useLocation();
+
+  // Scroll ke atas setiap kali halaman berganti
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen font-sans" style={{ backgroundColor: '#4EA674' }}>
       <Navbar />
